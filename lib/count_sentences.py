14034e73118fc7b2e59a4eb35f4c8127862a140e
JoeKaleli <kalelimburu9@gmail.com>
2024-03-19 11:19:02 +0300

    Only passed two of the tests

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
 
-class MyString:
-  pass
+class MyString():
+    
+
+    def __init__ (self, string):
+        self.string = string
+
+    def is_sentence(self):
+        if self.string.endswith('.'):
+            return True
+        else:
+          return False
+        
+    def is_question(self):
+        if self.string.endswith('?'):
+            return True
+        else:
+            return False
+        
+    def count_sentences(self):
+        # Split the string into sentences based on periods
+        sentences = self.value.split('.')
+        # Filter out any empty strings (e.g., caused by consecutive periods)
+        sentences = [sentence.strip() for sentence in sentences if sentence.strip()]
+        # Return the count of sentences
+        return len(sentences)
